01f66fe003023da4edf72bb424eecca77385221e
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-06-19 13:00:40 +0100

    tweak post route for new characters

--- backend/routes/characterRoutes.js
+++ backend/routes/characterRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 
-const { isLoggedIn } = require('../middleware/authMiddleware');
+const { isLoggedIn, isCharacterCreator } = require('../middleware/authMiddleware');
 
 const User = require('../models/User');
 const Campaign = require('../models/Campaign');
@@ -14,13 +14,21 @@ router.post('/', isLoggedIn, asyncHandler(async (req, res) => {
 		const character = new Character(req.body)
 		const user = await User.findById(req.user.id)
 		const campaign = await Campaign.findById(req.body.campaignId)
+
 		character.createdBy = user.id;
+		character.currentHp = req.body.maxHp
+		character.currentHitDice = req.body.maxHitDice
+
 		user.characters.push(character.id)
+		user.campaigns.push(campaign.id)
+
 		campaign.characters.push(character.id)
 		campaign.users.push(user.id)
+
 		await character.save()
 		await campaign.save()
 		await user.save()
+
 		res.status(201).json(character)
 	} catch (err) {
 		console.log(err)
