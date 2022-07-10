119b13f02a6fc24faa6981e00a105253405d4cb6
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-07-10 11:50:57 +0100

    add POST route for new customStat

--- backend/models/CustomStat.js
+++ backend/models/CustomStat.js
@@ -27,43 +27,10 @@ const CustomStatSchema = mongoose.Schema({
     type: Boolean,
     required: true,
   },
-  shortRestRestoreAmount: {
-    type: Number,
-    required: function checkRequired() {
-      if (this.shortRestRestore) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: {
-      validator: function (number) {
-        return number && number <= this.maxAmount && number > 0;
-      },
-      message:
-        'Short rest amount cannot be more than the maximum amount or less than 0',
-    },
-  },
   longRestRestore: {
     type: Boolean,
     required: true,
-  },
-  longRestRestoreAmount: {
-    type: Number,
-    required: function checkRequired() {
-      if (this.longRestRestore) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: {
-      validator: function (number) {
-        return number && number <= this.maxAmount && number > 0;
-      },
-      message: 'Long rest restore cannot exceed maximum amount or be less than 0',
-    },
-  },
+  }
 });
 
 module.exports = mongoose.model('CustomStat', CustomStatSchema);
