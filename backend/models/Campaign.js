dd83c52d12dea1f28e535f1be1ad7e0873db204e
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-07-10 14:43:27 +0100

    ensure customStats are deleted if a campaign is deleted

--- backend/models/Campaign.js
+++ backend/models/Campaign.js
@@ -53,31 +53,48 @@ const CampaignSchema = mongoose.Schema({
 
 CampaignSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
+    // Delete associated customStats
+    const characters = await Character.find({ _id: doc.characters})
+    const customStatIds = []
+    characters.forEach((character) => {
+			character.customStats.forEach((customStat) => {
+				customStatIds.push(customStat)
+			})
+		})
+    await CustomStat.deleteMany({
+      _id: customStatIds
+    })
+    // Delete associated characters
     await Character.deleteMany({
       _id: {
         $in: doc.characters,
       },
     });
+    // Delete associated lootItems
     await LootItem.deleteMany({
       _id: {
         $in: doc.lootItems,
       },
     });
+    // Delete associated magicItems
     await MagicItem.deleteMany({
       _id: {
         $in: doc.magicItems,
       },
     });
+    // Delete associated quests
     await Quest.deleteMany({
       _id: {
         $in: doc.quests,
       },
     });
+    // Delete associated NPCs
     await NonPlayableCharacter.deleteMany({
       _id: {
         $in: doc.nonPlayableCharacters,
       },
     });
+    // Update user to remove campaign and also associated character from respective arrays
     await User.updateMany(
       { campaigns: { $in: doc._id } },
       { $pull: { characters: { $in: doc.characters } } }
@@ -91,6 +108,7 @@ CampaignSchema.post('findOneAndDelete', async function (doc) {
 
 module.exports = mongoose.model('Campaign', CampaignSchema);
 const Character = require('./Character');
+const CustomStat = require('./CustomStat');
 const User = require('./User');
 const LootItem = require('./LootItem');
 const MagicItem = require('./MagicItem');
