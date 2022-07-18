f385a9e6d7d1588d904354606e470e69d9641d0b
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-07-18 22:31:36 +0100

    start wiring up register component to backend

--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,4 +1,37 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { register, reset } from '../../features/auth/authSlice';
+
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  // const onChange = (e) => {
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
+
+	// const onSubmit = (e) => {
+	// 	e.preventDefault()
+
+	// 	if(password !== confirmPassword) {
+
+	// 	}
+	// }
   return (
     <>
       <h2>I am the registration page</h2>
