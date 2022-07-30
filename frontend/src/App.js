93d626f097464a00bcc0a9daf2130b2959850ed0
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-07-30 14:56:38 +0100

    add basic private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,25 @@ import RegisterPage from './pages/RegisterPage/RegisterPage';
 import CampaignsHomePage from './pages/CampaignsHomePage/CampaignsHomePage';
 import CampaignDashboard from './pages/CampaignDashboard/CampaignDashboard';
 import Error from './pages/Error/Error';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="campaigns" element={<PrivateRoute />}>
             <Route path="campaigns" element={<CampaignsHomePage />} />
-            <Route path="campaigns/:id" element={<CampaignDashboard />} />
-            <Route path="*" element={<Error />} />
           </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+          <Route path="campaigns/:campaignId" element={<PrivateRoute />}>
+            <Route path="campaigns/:campaignId" element={<CampaignDashboard />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStatus } from '../../hooks/useAuthStatus';
+
+const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return <div>Loading</div>;
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
